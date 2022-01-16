a8f94fcecffbd23de200b93a05fe64d76044b871
Philipp Gerber <philipp@retinai.com>
2022-01-16 13:03:37 +0100

    feat: initial commit, add tailwindcss

---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import type { MetaFunction } from "remix";
+import { Link } from "remix";
+import styles from "./tailwind.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Link to="/posts">Posts</Link>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Posts() {
+  return <h1 className="text-4xl text-red-400">Posts</h1>;
+}
