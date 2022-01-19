84f4a8e1257bae5e2591650802e2ff9f9ab9f440
Philipp Gerber <philipp@retinai.com>
2022-01-19 09:46:05 +0100

    feat: delete unnecessary code

--- app/utils/file.server.ts
+++ app/utils/file.server.ts
@@ -1,25 +1,9 @@
-import { UploadHandler } from "@remix-run/node/formData";
-import fs from "fs";
-import path from "path";
 import {
   unstable_createFileUploadHandler,
   unstable_parseMultipartFormData,
 } from "remix";
 
 const UPLOAD_DIR = "public/uploads";
-
-// export const uploadHandler: UploadHandler = async ({ stream, filename }) => {
-//   const fileURL = `uploads/${filename}`;
-//   const filepath = path.resolve(UPLOAD_DIR, filename);
-
-//   // Get the file as a buffer
-//   const chunks = [];
-//   for await (const chunk of stream) chunks.push(chunk);
-//   const buffer = Buffer.concat(chunks);
-
-//   fs.writeFileSync(filepath, buffer);
-//   return fileURL;
-// };
 
 export const uploadHandler = unstable_createFileUploadHandler({
   directory: UPLOAD_DIR,
