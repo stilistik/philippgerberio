35db55105cec45ae42a65a2b1cab90c50b17aaeb
Philipp Gerber <philipp@retinai.com>
2022-01-20 14:49:11 +0100

    feat: store resources on upload

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,10 +1,26 @@
+import { NodeOnDiskFile } from "@remix-run/node";
 import { ActionFunction, redirect } from "remix";
+import { db } from "~/utils/db.server";
 import { parseFormData } from "~/utils/file.server";
+import { badRequest } from "~/utils/routing.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const data = await parseFormData(request);
   const file: any = data.get("file");
-  return `/uploads/${file.name}`;
+
+  if (!(file instanceof NodeOnDiskFile)) {
+    return badRequest("Form not submitted correctly.");
+  }
+
+  const resource = await db.resource.create({
+    data: {
+      name: file.name,
+      url: `/uploads/${file.name}`,
+      mimetype: file.type,
+    },
+  });
+
+  return resource.url;
 };
 
 export const loader = () => {
