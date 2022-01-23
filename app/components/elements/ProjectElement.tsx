4874bde43be09a9c64b10a05e77b13368ba89536
Philipp Gerber <philipp@retinai.com>
2022-01-23 21:34:11 +0100

    feat: add cool project effect

--- app/components/elements/ProjectElement.tsx
+++ app/components/elements/ProjectElement.tsx
@@ -13,30 +13,43 @@ export const ProjectElement: React.FC<ProjectProps> = ({
   children,
 }) => {
   const [hovered, setHovered] = React.useState(false);
+  const [position, setPosition] = React.useState({ x: 0, y: 0 });
+
+  function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
+    const bbox = e.currentTarget.getBoundingClientRect();
+    const x = ((e.pageX - bbox.x) / bbox.width) * 100;
+    const y = ((e.pageY - bbox.y) / bbox.height) * 100;
+
+    setPosition({ x, y });
+    setHovered(true);
+  }
 
   return (
     <div
       className="project-element"
-      onMouseEnter={() => setHovered(true)}
+      onMouseMove={handleMouseMove}
       onMouseLeave={() => setHovered(false)}
     >
       <div className="project-border project-border-top" />
       <div className="project-border project-border-left" />
       <div className="project-border project-border-bottom" />
+      <div className="project-border project-border-right" />
       <div
-        className="bg-black absolute bottom-1/2 right-0 rounded-full transition-all duration-1000"
+        className="bg-black absolute rounded-full transition-transform origin-center duration-1000"
         style={{
+          top: position?.y + "%",
+          left: position?.x + "%",
           width: "100%",
           height: "100%",
-          transform: `translate(50%, 50%) scale(${hovered ? 1.6 : 0})`,
+          transform: `translate(-50%, -100%) scale(${hovered ? 1.6 : 0})`,
         }}
       />
       {project.thumbnail ? (
-        <div className="rounded-md bg-black">
+        <div className="rounded-md" style={{ background: "#333" }}>
           <img
             src={project.thumbnail}
             className={clx(
-              "rounded-md mix-blend-lighten transition-all duration-1000",
+              "rounded-md mix-blend-exclusion border transition-all duration-1000",
               {
                 grayscale: !hovered,
               }
