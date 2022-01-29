aa1a11af2f3ce9ed2043639858265f4b8ece7bf3
Philipp Gerber <philipp@retinai.com>
2022-01-29 14:22:30 +0100

    feat: technology word cloud

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import clx from "classnames";
+import { Button } from "~/components/interaction/Button";
+import { Link } from "remix";
 
 const useScrollPosition = () => {
   const ref = React.useRef<HTMLDivElement>(null);
@@ -156,7 +158,7 @@ const PictureSection = () => {
   const { ref, percent } = useScrollPosition();
   return (
     <section ref={ref} className="w-full h-[300vh]">
-      <div className="sticky top-0 px-60 ">
+      <div className="sticky top-0 px-60">
         <div
           className="absolute top-0 left-0 flex flex-col gap-10 items-center p-10 justify-center"
           style={{
@@ -206,28 +208,100 @@ const PictureSection = () => {
 };
 
 const technologies = [
-  "React",
-  "Remix",
-  "NextJS",
-  "Vue",
-  "Node",
-  "Postgres",
-  "DynamoDB",
-  "JavaScript",
-  "TypeScript",
-  "Python",
-  "Docker",
-  "C#",
-  "C++",
-  "SQL",
+  { name: "React", size: 12 },
+  { name: "JavaScript", size: 8 },
+  { name: "TypeScript", size: 6 },
+  { name: "Vue", size: 6 },
+  { name: "Node", size: 7 },
+  { name: "Python", size: 7 },
+  { name: "Remix", size: 7 },
+  { name: "Docker", size: 7 },
+  { name: "NextJS", size: 6 },
+  { name: "Postgres", size: 6 },
+  { name: "C#", size: 6 },
+  { name: "SQL", size: 6 },
+  { name: "DynamoDB", size: 3 },
+  { name: "C++", size: 1 },
+  { name: "Java", size: 1 },
+  { name: "Unity 3D", size: 1 },
 ];
 
 const TechnologySection = () => {
   const { ref, percent } = useScrollPosition();
-  const { width, height } = useWindowDimensions();
+  const wordRefs = React.useRef<HTMLDivElement[]>([]);
+
+  React.useLayoutEffect(() => {
+    const boundingBoxes: DOMRect[] = [];
+
+    const baseIncrement = 20;
+    const incrementSteps = 50;
+    const angularSteps = 50;
+
+    function isIntersecting(a: DOMRect) {
+      if (
+        a.x < 0 ||
+        a.x + a.width > window.innerWidth ||
+        a.y < 0 ||
+        a.y + a.height > window.innerHeight
+      ) {
+        return true;
+      }
+      for (const b of boundingBoxes) {
+        if (
+          a.x <= b.x + b.width &&
+          a.x + a.width >= b.x &&
+          a.y <= b.y + b.height &&
+          a.y + a.height >= b.y
+        ) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function positionElement(div: HTMLDivElement) {
+      const currentX = Number(div.style.left.replace("px", ""));
+      const currentY = Number(div.style.top.replace("px", ""));
+      for (let i = 0; i < incrementSteps; ++i) {
+        const r = baseIncrement * i;
+        for (let a = 0; a < angularSteps; ++a) {
+          const angle = ((2 * Math.PI) / angularSteps) * a;
+          const dx = Math.sin(angle) * r;
+          const dy = Math.cos(angle) * r;
+          console.log(dx, dy);
+
+          div.style.top = `${currentY + dy}px`;
+          div.style.left = `${currentX + dx}px`;
+
+          const bbox = div.getBoundingClientRect();
+          const intersects = isIntersecting(bbox);
+          if (!intersects) return bbox;
+        }
+      }
+      return null;
+    }
+
+    wordRefs.current.forEach((div, idx) => {
+      const tech = technologies.find((el) => el.name === div.id);
+      if (!tech) return;
+
+      div.style.top = window.innerHeight / 2 + "px";
+      div.style.left = window.innerWidth / 2 + "px";
+      div.style.fontSize = `${tech.size}em`;
+      div.style.transform = `translate(-50%, -50%)`;
+
+      if ((idx + 1) % 2 === 0) {
+        div.style.transform += " rotate(-90deg)";
+      }
+
+      const bbox = positionElement(div);
+      if (bbox) boundingBoxes.push(bbox);
+    });
+  }, []);
+
   return (
     <section ref={ref} className="w-full h-[300vh]">
-      <div className="sticky top-0 p-20 gap-10 bg-white">
+      <div className="sticky top-0 p-20 gap-10 bg-white h-screen w-screen overflow-hidden flex items-center">
         <div
           className="absolute top-0 rounded-full bg-black flex items-center justify-center shadow-2xl"
           style={{
@@ -239,37 +313,66 @@ const TechnologySection = () => {
             }%) scale(${0.1 + lerp(percent, 0, 0.5) * 3})`,
           }}
         />
-        <div className="flex flex-wrap gap-2">
-          {technologies.map((tech, i) => {
+        <div className="flex flex-wrap gap-2 justify-center items-baseline">
+          {technologies.map(({ size, name }, i) => {
             const t_in = 0.6;
             const inc_in = t_in / technologies.length;
             const inc_out = 0.4 / technologies.length;
 
             return (
-              <span
-                className={clx(
-                  "relative px-8 py-3 font-thin text-7xl lg:text-8xl 2xl:text-9xl mix-blend-difference bg-black text-white border-white border-4"
-                )}
-                style={{
-                  opacity:
-                    lerp(percent, i * inc_in, (i + 1) * inc_in) -
-                    lerp(percent, t_in + i * inc_out, t_in + (i + 1) * inc_out),
-                  transform: `translate(${
-                    10 -
-                    lerp(percent, i * inc_in, (i + 1) * inc_in) * 10 -
-                    lerp(
-                      percent,
-                      t_in + i * inc_out,
-                      t_in + (i + 1) * inc_out
-                    ) *
-                      10
-                  }%, 0)`,
+              <div
+                key={name}
+                id={name}
+                ref={(ref) => {
+                  if (ref) wordRefs.current.push(ref);
                 }}
+                className="absolute mix-blend-difference text-white p-3"
               >
-                {tech}
-              </span>
+                <span
+                  className="p-2 border-4 border-white leading-none"
+                  style={{
+                    opacity:
+                      lerp(percent, i * inc_in, (i + 1) * inc_in) -
+                      lerp(
+                        percent,
+                        t_in + i * inc_out,
+                        t_in + (i + 1) * inc_out
+                      ),
+                    transform: `scale(${
+                      1 -
+                      lerp(percent, i * inc_in, (i + 1) * inc_in) * 0.2 -
+                      lerp(
+                        percent,
+                        t_in + i * inc_out,
+                        t_in + (i + 1) * inc_out
+                      ) *
+                        0.2
+                    }`,
+                  }}
+                >
+                  {name}
+                </span>
+              </div>
             );
           })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const FinalSection = () => {
+  const { ref, percent } = useScrollPosition();
+  return (
+    <section ref={ref} className="w-full h-[300vh]">
+      <div className="sticky top-0 p-20 gap-10 bg-white">
+        <div className="flex gap-10">
+          <Link to="/projects">
+            <Button size="large">See the projects</Button>
+          </Link>
+          <Link to="/posts">
+            <Button size="large">Read the blog</Button>
+          </Link>
         </div>
       </div>
     </section>
@@ -282,6 +385,7 @@ export default function Index() {
       <TitleSection />
       <PictureSection />
       <TechnologySection />
+      <FinalSection />
     </div>
   );
 }
