70810e9a4d9432d45c1cde832e0f30d814219ae4
Philipp Gerber <philipp@rundumeli.ch>
2022-06-19 22:09:29 +0200

    feat: add the anchor tool

--- app/components/wysiwyg/EditorTools.tsx
+++ app/components/wysiwyg/EditorTools.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import { Link } from "@remix-run/react";
 import { BoldIcon } from "~/icons/Bold";
 import { ItalicIcon } from "~/icons/Italic";
@@ -16,6 +16,9 @@ import { TextIcon } from "~/icons/Text";
 import { OrderedListIcon } from "~/icons/OrderedList";
 import { UnorderedListIcon } from "~/icons/UnorderedList";
 import { useEditorContext } from "./Editor";
+import { LinksIcon } from "~/icons/Links";
+import { Input } from "../interaction/Input";
+import { useEventListener } from "~/utils/hooks";
 
 export const EditorTools = () => {
   return (
@@ -29,6 +32,7 @@ export const EditorTools = () => {
       <H1 />
       <H2 />
       <H3 />
+      <Anchor />
       <OrderedList />
       <UnorderedList />
       <Code />
@@ -96,5 +100,82 @@ const Image = () => {
         <ImageIcon />
       </IconButton>
     </Link>
+  );
+};
+
+const Anchor = () => {
+  const [anchor, setAnchor] = React.useState<HTMLButtonElement | null>(null);
+  const editor = useEditorContext();
+  const [url, setUrl] = React.useState("");
+  const [text, setText] = React.useState("");
+  const rangeRef = React.useRef<Range | null>(null);
+
+  if (typeof document !== "undefined") {
+    useEventListener(document, "keydown", (e) => {
+      if (e.key === "Escape") {
+        setAnchor(null);
+      }
+    });
+  }
+
+  function handleClick(e: any) {
+    e.preventDefault();
+    const selection = window.getSelection();
+    if (selection && selection.rangeCount !== 0) {
+      const range = selection.getRangeAt(0).cloneRange();
+      rangeRef.current = range;
+    }
+    setAnchor(e.currentTarget);
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent) {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  }
+
+  function handleSubmit() {
+    if (url && text) {
+      const html = `<a href=${url}>${text}</a>`;
+      if (rangeRef.current) {
+        const selection = window.getSelection();
+        selection?.removeAllRanges();
+        selection?.addRange(rangeRef.current);
+        editor?.focus();
+      }
+      document.execCommand("insertHTML", false, html);
+    }
+    setAnchor(null);
+  }
+
+  const bbox = anchor?.getBoundingClientRect();
+
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <LinksIcon />
+      </IconButton>
+      {anchor && (
+        <div
+          style={{ top: bbox?.bottom, left: bbox?.left }}
+          className="z-20 fixed bg-white shadow-lg rounded-lg p-3"
+        >
+          <Input
+            name="url"
+            placeholder="URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <Input
+            name="text"
+            placeholder="Text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      )}
+    </>
   );
 };
